81a4b50f29ab70d2673a827742b0178dc4ae8463
Jean Abayo <jeanabayo@Jeans-MacBook-Pro.local>
2017-11-20 14:23:32 +0300

    Add tests for models

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -9,7 +9,18 @@ class ModelsTestCase(unittest.TestCase):
 	"""Models Test Case"""
 
 	def setUp(self):
-		pass
+		self.app = create_app(config_name="testing")
+		self.user = Users(username="John Doe", email="johndoe@sl.com", password="secret")
+		with self.app.app_context():
+			# create all tables
+			db.create_all()
 
 	def test_user_model(self):
 		pass
+
+	def tearDown(self):
+	    """teardown all initialized variables."""
+	    with self.app.app_context():
+	        # drop all tables
+	        db.session.remove()
+	        db.drop_all()
